05c4b9916208e7a85bbc13280d49103214908618
UjjvalDhakad12 <allinonefan9@gmail.com>
2024-10-19 17:00:49 +0530

    update

--- src/Hero.jsx
+++ src/Hero.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import './Hero.css'
+import { TruckOutlined } from '@ant-design/icons';
 
 function Hero() {
     return (
@@ -7,7 +8,11 @@ function Hero() {
             <div>
                 <p className='head1'>Lowest Prices
                     <br />Best Quality Shopping</p>
-                <img src="public\hero1.png" className='head1-img'/>
+                <img src="public\hero1.png" className='head1-img' />
+                <div className="hero-p-itme">
+                    <p><TruckOutlined /></p>
+                </div>
+
             </div>
         </div>
     )
