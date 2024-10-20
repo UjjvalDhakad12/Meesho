0caff720e2d38068c66bef4b06d45bcf65487411
UjjvalDhakad12 <allinonefan9@gmail.com>
2024-10-20 13:59:18 +0530

    update

--- src/Hero.jsx
+++ src/Hero.jsx
@@ -1,21 +1,24 @@
 import React from 'react'
 import './Hero.css'
-import { TruckOutlined } from '@ant-design/icons';
+
 
 function Hero() {
     return (
         <div>
-            <div>
-                <p className='head1'>Lowest Prices
-                    <br />Best Quality Shopping</p>
-                <img src="public\hero1.png" className='head1-img' />
-                <div className="hero-p-itme">
-                    <p><TruckOutlined /></p>
+            <div className="left">
+                <p className='heading'>Lowest Prices<br />
+                    Best Quality Shopping</p>
+                <div className="item">
+                    <pre><i class="fa-solid fa-truck"></i> Free Delivery</pre>
+                    <pre><i class="fa-solid fa-hand-holding-dollar"></i> Cash on Delivery</pre>
+                    <pre><i class="fa-solid fa-person-walking-arrow-loop-left"></i> Easy Returns</pre>
                 </div>
-
+                <button className='btn'>
+                    <i class="fa-brands fa-google-play"></i>
+                    Download the Meesho App</button>
             </div>
         </div>
     )
 }
 
-export default Hero;
+export default Hero
